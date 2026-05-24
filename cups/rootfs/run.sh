bb7a55ca7aee4c0f52ce84272d01d01afc2576de
Domokos Décsi <57868425+domokosdcs0@users.noreply.github.com>
2026-05-24 19:10:42 +0200

    Enhance run.sh to clean up old PIDs and start services
    Signed-off-by: Domokos Décsi <57868425+domokosdcs0@users.noreply.github.com>

--- cups/rootfs/run.sh
+++ cups/rootfs/run.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
-# Wait for Avahi to be ready
+bashio::log.info "Cleaning up old Avahi and CUPS PIDs..."
+rm -rf /var/run/avahi-daemon/*
+rm -f /var/run/cups/cupsd.pid
+
+if ! pidof avahi-daemon > /dev/null; then
+  bashio::log.info "Starting Avahi daemon..."
+  avahi-daemon --daemonize --no-chroot
+fi
+
+bashio::log.info "Waiting for Avahi socket..."
 until [ -e /var/run/avahi-daemon/socket ]; do
   sleep 1s
 done
 
 bashio::log.info "Starting CUPS server..."
-
-cupsd -f
+exec cupsd -f
